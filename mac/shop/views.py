962a021385d25b7547add8327173670838354bba
manishmeshram51 <manishmeshram51@gmail.com>
2020-06-09 04:01:46 +0530

    upadated carousal with multiople categories

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -5,13 +5,23 @@ import math
 
 def index(request):
     products = Product.objects.all()
-    
+    print(products)
     n = len(products)
-
     nSlides = n//4 + math.ceil((n/4)-(n//4))
-    print(n, nSlides)
-    params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
-    return render(request,'shop/index.html',params)
+    allProds = []
+    # catprods = Product.objects.values('category', 'id')
+    # cats = {item['category'] for item in catprods}
+    # for cat in cats:
+    #     prod = Product.objects.filter(category=cat)
+    #     n = len(prod)
+    #     nSlides = n // 4 + ceil((n / 4) - (n // 4))
+    #     allProds.append([prod, range(1, nSlides), nSlides])
+
+
+    allProds = [[products, range(1, nSlides), nSlides],
+                [products, range(1, nSlides), nSlides]]
+    params = {'allProds':allProds}
+    return render(request, 'shop/index.html', params)
 
 def about(request):
     return render(request,'shop/about.html')
